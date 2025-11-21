2d4a154bcfaea1f66bc42ae8d22afbdb06037e97
shotzzydev <luanventuradev@gmail.com>
2025-11-21 15:58:33 -0300

    new exercise

---
+++ aula41/index.js
@@ -0,0 +1,15 @@
+// Escreva uma funcao que recebe 2 numeros e retorne o mair deles
+
+const max = (x, y) => x > y ? x : y
+
+console.log(max(10, 15));
+
+// function maiorNumero(x, y) {
+//     if (x > y) {
+//         return x
+//     }
+
+//     if (y > x) {
+//         return y
+//     }
+// }
