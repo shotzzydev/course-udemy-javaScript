0c92ecc2556a65ea9a81090aa10c2f9b0b74ff59
shotzzydev <luanventuradev@gmail.com>
2025-11-25 03:57:49 -0300

    feat: new exercise

--- aula41/index.js
+++ aula41/index.js
@@ -1,8 +1,8 @@
 // Escreva uma funcao que recebe 2 numeros e retorne o mair deles
 
-const max = (x, y) => x > y ? x : y
+// const max = (x, y) => x > y ? x : y
 
-console.log(max(10, 15));
+// console.log(max(10, 15));
 
 // function maiorNumero(x, y) {
 //     if (x > y) {
@@ -13,3 +13,17 @@ console.log(max(10, 15));
 //         return y
 //     }
 // }
+
+const flutas = [0, 1, 2];
+
+// for(i = 0; i < flutas.length; i++) {
+//     console.log(flutas[i])
+// }
+
+// let i = 0;
+
+// do {
+//     console.log(i); 
+//     ++i
+// } while (i < flutas.length) 
+
---
+++ aula42/index.js
@@ -0,0 +1,10 @@
+// Escreva um funcao chamada ePaisagem que
+// Receber dois argumentos, lagura e altura
+// de uma imagem (number)
+// Retorne se a true se a imagem estive
+// no modo paisagem
+
+
+const ePaisagem = (width, height) => width > height 
+
+console.log(ePaisagem(1080, 1920));
