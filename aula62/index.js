81616c08baacd5b871aa8f82d6feca2ad01e0c7d
shotzzydev <luanventuradev@gmail.com>
2025-12-12 14:54:54 -0300

    feat: new class Review of Array Basics

---
+++ aula62/index.js
@@ -0,0 +1,27 @@
+// Valor por referencia
+//               0          1          2         3           4
+
+// const nome = 'Austin Oliveira';
+// const nomes = nome.split(' ');
+
+const nomes = [ 'Austin', 'Oliveira' ]
+const nome = nomes.join(' ');
+
+console.log(nome);
+
+// const nomes = ["Austin", "Ashley", "Manuela", 'Wallace', 'Nguyen'];
+// const novo = nomes.slice(0, -1 );
+// console.log(novo);
+
+// nomes.unshift('João');
+// nomes.unshift('Wallace'); // Adiciona no incio da array
+
+// const removido = nomes.shift(); // Remove do inicio
+// const removido = nomes.pop(); // Remove do final
+
+// const nomes = ["Austin", "Ashley", "Manuela"];
+// novo.pop();
+
+// console.log(nomes);
+// const novo = [...nomes];
+// console.log(novo);
